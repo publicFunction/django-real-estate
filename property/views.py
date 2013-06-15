9d64045db96744f6919d8c4a266aa7fae46760df
Chris Moss <chris@uniontech.co.uk>
2013-06-15 22:30:21 +0100

    removed unwated template variabel from view
    Signed-off-by: Chris Moss <chris@uniontech.co.uk>

--- property/views.py
+++ property/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 from property.models import Property, FeaturedProperty
-from realestate.settings import MEDIA_URL 
 
 def index(request):
 	featuredList = []
 	for fp in FeaturedProperty.objects.all().order_by('?')[:2]:
 		fp = Property.objects.get(pk=fp.property_id)
 		featuredList.append(fp)
+	featured = {'featured' : featuredList}
+	
 	return render_to_response(  'home/index.html',
-								{   'featured': featuredList,
-									'media_url' : MEDIA_URL
+								{
+									'featured': featured,
 								},
-								context_instance=RequestContext(request))
+								context_instance=RequestContext(request)
+							)
