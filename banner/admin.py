64047bbce694d3754c3eb9cac3c4860363f42461
publicFunction <chris@publicfunction.co.uk>
2013-06-16 19:41:21 +0100

    Added support for ckeditor in the banner module...
    Signed-off-by: publicFunction <chris@publicfunction.co.uk>

--- banner/admin.py
+++ banner/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
 from django.db import models
+from django import forms
+
+from ckeditor.widgets import CKEditorWidget
 
 from realestate import settings
 from models import Banner, BannerImages
 
+class BannerAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorWidget())
+    class Meta:
+        model = BannerImages
+
 class BannerImagesInline(admin.StackedInline):
     model = BannerImages
-    
+    form = BannerAdminForm
     
 class BannerAdmin(admin.ModelAdmin):
     inlines = [BannerImagesInline,]
